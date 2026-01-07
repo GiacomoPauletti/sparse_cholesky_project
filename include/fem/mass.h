5b73a77ab8ca80c49c3480f73ee260d7e00f9d23
didiersmets <didier.smets@sorbonne-universite.fr>
2026-01-07 11:29:46 +0100

    Revert "modification fem/stiffness.h et fem/mass.h"
    This reverts commit 73d901491e4c3da4976e860d49a449d603ad2502.

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -17,11 +17,5 @@
  */
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-// Attention il faut entrer une Matrice M de type P1_cst
-	// Aire du triangle ABC
-	const double area = 0.5 * norm(cross(AB, AC));
-
-	M[0] = area/6.0
-	M[1] = area/12.0 //coef des côtés
-
+	/* Your implementation goes here ! */
 }
